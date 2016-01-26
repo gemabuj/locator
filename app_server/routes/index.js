5bec6953c13429a000e2403c7747b3741218a16e
gemabuj <gema.geologist@yahoo.co.id>
2016-01-26 19:34:53 +0700

    Tidying index.js

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-/* var ctrlMain = require('../controllers/main'); */
 var ctrlLocations = require('../controllers/locations');
 var ctrlOthers = require('../controllers/others');
 
@@ -13,19 +12,3 @@ router.get('/location/review/new', ctrlLocations.addReview);
 router.get('/about', ctrlOthers.about);
 
 module.exports = router;
-
-/* GET home page.
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-*/
-
-/* Defining as a function 
-var homepageController = function(req, res){
-	res.render('index', {title: 'Express Yeah'});
-};
-*/
-
-/* GET homepage */
-/* router.get('/', ctrlMain.index); */
-

55333b9e1e4710a4968615c30fffc0d777da55ef
gemabuj <gema.geologist@yahoo.co.id>
2016-01-26 19:20:37 +0700

    Creating 4 basic controller functions such as about, homelist, locationInfo and addReview

---
+++ app_server/controllers/locations.js
@@ -0,0 +1,14 @@
+/* GET 'homelist' page */
+module.exports.about = function(req, res){
+	res.render('index', {title: 'About this app'});
+};
+
+/* GET 'location info' page */
+module.exports.about = function(req, res){
+	res.render('index', {title: 'About this app'});
+};
+
+/* GET 'add review' page */
+module.exports.about = function(req, res){
+	res.render('index', {title: 'About this app'});
+};

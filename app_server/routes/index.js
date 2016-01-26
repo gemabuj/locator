b0bfb61df7bf3746eef5cce1b37071b5bf6eb8e9
gemabuj <gema.geologist@yahoo.co.id>
2016-01-26 11:29:58 +0700

    Splitting controllers from routes

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET home page.
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+*/
+
+/* Defining as a function */
+var homepageController = function(req, res){
+	res.render('index', {title: 'Express Yeah'});
+};
+
+/* GET homepage */
+router.get('/', homepageController);
 
 module.exports = router;

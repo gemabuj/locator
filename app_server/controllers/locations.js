0ecbb1b61876aeff1397d94736363e6073d83a4e
gemabuj <gema.geologist@yahoo.co.id>
2016-01-28 18:16:52 +0700

    Adding title, pageHeader, sidebar and locations from view (locations-list.jade) into controller (location.js)

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -5,7 +5,24 @@ module.exports.homelist = function(req, res){
     pageHeader: {
       title: 'LocATor',
       strapline: 'Find places to work with wifi near you!'
-    }
+    },
+    sidebar: 'Looking for wifi and a seat? LocATor helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let LocATor help you find the place you are looking for.',
+    locations: [{
+      name: 'The Captain',
+      address: 'Damai Street Krikilan Ngaglik Sleman 55581',
+      rating: 4,
+      facilities: ['Lounge','Live Music','Wifi','Security','Car Park']
+    },{
+      name: 'Kedai 24 Jam',
+      address: 'Prujakan 22B Ngaglik Sleman 56755',
+      rating: 1,
+      facilities: ['Fake Bar', 'Crappy Food and Drink', 'Wifi', 'Noisy']
+    },{
+      name: 'Kedai Lumer',
+      address: 'Kaliurang Street 8.7 Ngaglik Sleman 55565',
+      rating: 4,
+      facilities: ['Easy Dining', 'Friendly Waitresses', 'Wifi', 'Cozy']
+    }]
   });
 };
 

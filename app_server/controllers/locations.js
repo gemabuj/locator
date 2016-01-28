915c3ef1793819a271be893a672a78d16de802db
gemabuj <gema.geologist@yahoo.co.id>
2016-01-28 17:40:59 +0700

    Moving static data (texts) from the view (locations-list.jade) to the controller (locations.js)

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,14 +1,20 @@
 /* GET 'homelist' page with locationslist.jade layout */
 module.exports.homelist = function(req, res){
-	res.render('locations-list', {title: 'Home'});
+  res.render('locations-list', {
+    title: 'LocATor - find a place to work with wifi',
+    pageHeader: {
+      title: 'LocATor',
+      strapline: 'Find places to work with wifi near you!'
+    }
+  });
 };
 
 /* GET 'location info' page */
 module.exports.locationInfo = function(req, res){
-	res.render('locations-info', {title: 'List of Locations'});
+  res.render('locations-info', {title: 'List of Locations'});
 };
 
 /* GET 'add review' page */
 module.exports.addReview = function(req, res){
-	res.render('locations-review-form', {title: 'Add Your Review'});
+  res.render('locations-review-form', {title: 'Add Your Review'});
 };

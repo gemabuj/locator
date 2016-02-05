11b6789032418dfd90a8a516278f24adc03b5722
gemabuj <gema.geologist@yahoo.co.id>
2016-02-05 17:37:50 +0700

    Adding variable mongoose, Loc and sendJsonResponse on the top of location.js app_api controller

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -1,3 +1,14 @@
+/* Sample of module.exports controller
 module.exports.add.locationsCreate = function (req, res){
-	
+}; */
+
+var mongoose = require('mongoose');
+var Loc = mongoose.model('Location');
+var sendJsonResponse = function(res, status, content) {
+  res.status(status);
+  res.json(content);
+};
+
+module.exports.locationsCreate = function (req, res) {
+  sendJsonResponse(res, 200, {"status" : "success"});
 };
